d77beedbb62c34e189f45fa791c0054d22d3c53b
Maciej Grs <maciejgrs@gmail.com>
2021-03-15 11:18:01 +0100

    build the qoute on click

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+ const author = document.querySelector('#author')
+ const quote = document.querySelector('#quote')
+ const newQuote = document.querySelector('#new-quote')
+ 
  let qoutes = []
+  
 
 const fetchQoute = async () => {
     const url = 'https://type.fit/api/quotes'
     try {
            const response = await fetch(url)
            qoutes = await response.json()
-           const random = qoutes[Math.floor(Math.random()*qoutes.length)];
-           console.log(random.text);
+           
+           
+            buildQoute()
     }
     catch (err) {
          console.log(err);
     }
 }
+ 
+ 
+ 
 
-fetchQoute()
+const buildQoute = () => {
+    const random = qoutes[Math.floor(Math.random()*qoutes.length)]
+    author.innerText = random.author
+    quote.innerText = random.text
+}
+
+newQuote.addEventListener('click', () =>  {
+    fetchQoute()
+})
+  
