12d98c3f7fe4c51aa53c9ba4a7ec2d96f9e2043f
Maciej Grs <maciejgrs@gmail.com>
2021-03-15 11:36:21 +0100

    check for author and text length

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
- const author = document.querySelector('#author')
- const quote = document.querySelector('#quote')
- const newQuote = document.querySelector('#new-quote')
- 
- let qoutes = []
-  
+const author = document.querySelector("#author");
+const quote = document.querySelector("#quote");
+const newQuote = document.querySelector("#new-quote");
+const twitterBtn = document.querySelector(".twitter-button");
+
+let qoutes = [];
 
 const fetchQoute = async () => {
-    const url = 'https://type.fit/api/quotes'
-    try {
-           const response = await fetch(url)
-           qoutes = await response.json()
-           
-           
-            buildQoute()
-    }
-    catch (err) {
-         console.log(err);
-    }
-}
- 
- 
- 
+  const url = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(url);
+    qoutes = await response.json();
+
+    buildQoute();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const buildQoute = () => {
-    const random = qoutes[Math.floor(Math.random()*qoutes.length)]
-    author.innerText = random.author
-    quote.innerText = random.text
-}
+  const random = qoutes[Math.floor(Math.random() * qoutes.length)];
+  if (!random.author) {
+    author.innerText = "Unknown";
+  } else {
+    author.innerText = random.author;
+  }
+  if (random.text.length > 50) {
+   
+    quote.classList.add('long-quote')
+  } else {
+    quote.classList.remove('long-quote')
+  }
+  quote.innerText = random.text;
+};
 
-newQuote.addEventListener('click', () =>  {
-    fetchQoute()
-})
-  
+newQuote.addEventListener("click", () => {
+  fetchQoute();
+});
