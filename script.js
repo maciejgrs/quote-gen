46a253f0e35a7bc1fa5251eb415f401d22943f8e
Maciej Grs <maciejgrs@gmail.com>
2021-03-15 12:02:13 +0100

    loader animation css/js

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const author = document.querySelector("#author");
 const quote = document.querySelector("#quote");
 const newQuote = document.querySelector("#new-quote");
 const twitterBtn = document.querySelector(".twitter-button");
+const loader = document.querySelector('#loader')
+const quoteContainer = document.querySelector('.quote-container');
 
 let qoutes = [];
 
 const fetchQoute = async () => {
+    loadings(false, true)
   const url = "https://type.fit/api/quotes";
   try {
     const response = await fetch(url);
     qoutes = await response.json();
 
-    buildQoute();
+    setTimeout(() =>
+    {
+        buildQoute()
+    }, 800)
+     
+    
   } catch (err) {
     console.log(err);
   }
@@ -25,14 +33,28 @@ const buildQoute = () => {
     author.innerText = random.author;
   }
   if (random.text.length > 50) {
-   
-    quote.classList.add('long-quote')
+    quote.classList.add("long-quote");
   } else {
-    quote.classList.remove('long-quote')
+    quote.classList.remove("long-quote");
   }
   quote.innerText = random.text;
+  loadings(true, false)
 };
 
-newQuote.addEventListener("click", () => {
-  fetchQoute();
-});
+const tweetTheQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.innerText} - ${author.innerText}`;
+  window.open(twitterUrl, "_blank");
+};
+
+
+const loadings = (bool, bool2) => {
+    loader.hidden = bool
+    quoteContainer.hidden = bool2   
+}
+ 
+newQuote.addEventListener("click", fetchQoute);
+
+twitterBtn.addEventListener("click", tweetTheQuote);
+
+fetchQoute()
+
